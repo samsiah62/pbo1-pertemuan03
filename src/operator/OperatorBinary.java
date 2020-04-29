315aff0d2f46d6421d4f5ca3554de663e246c053
samsiah <samsiahnonregbjb@gmail.com>
2020-04-29 13:08:01 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menampilkan nilai kedalam variabel
+    //baris 5-7 adalah commant/komentar yang tidak dijalankan oleh program
+
+    String nama = "Samsiah";
+    int jumlahSks = 72;
+    double indeksPredikatKomulatif = 3.38;
+
+    
+}
